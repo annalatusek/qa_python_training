3031ec0576f5ffdb9fc10ace5e94649b4d08051c
Anna <anna.latusek.21cn@gmail.com>
2018-10-10 14:52:01 +0200

    webdriver changed - instead of Firefox there is Chrome running

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,8 @@ from fixture.contact import ContactHelper
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Firefox()
+        # self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome(executable_path=r"C:\TestFiles\chromedriver.exe")
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
