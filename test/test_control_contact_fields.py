ecd44e4ff557a90e32cfae8d8cb78c0c3649bac7
Anna <anna.latusek.21cn@gmail.com>
2018-11-19 11:58:54 +0100

    Lesson 7, homework 21

--- test/test_control_contact_fields.py
+++ test/test_control_contact_fields.py
@@ -3,6 +3,26 @@ from random import randrange
 import re
 
 
+def test_contact_compare_home_page_with_db_data(app, db):
+    contact = Contact(firstname="Anna", lastname="Latusek", address="Klonowa, Katowice", home="555666",
+                      mobile="333444555", work="tester", email="a@gmail.com", email2="ab@gmail.com",
+                      email3="abc@gmail.com", phone2="456456")
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(contact))
+    contacts_from_home_page = app.contact.get_contact_list()
+    contacts_from_db = db.get_contact_list()
+    for contact_from_home_page in contacts_from_home_page:
+        for contact_from_db in contacts_from_db:
+            if contact_from_home_page.id == contact_from_db.id:
+                assert contact_from_home_page.firstname == contact_from_db.firstname
+                assert contact_from_home_page.lastname == contact_from_db.lastname
+                assert contact_from_home_page.address == contact_from_db.address
+                assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(
+                    contact_from_db)
+                assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(
+                    contact_from_db)
+
+
 def test_contact_compare_home_and_view_page(app):
     contact = Contact(firstname="Anna", lastname="Latusek", address="Klonowa, Katowice", home="555666",
                             mobile="333444555", work="tester", email="a@gmail.com", email2="ab@gmail.com",
